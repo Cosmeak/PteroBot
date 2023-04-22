7ad5c0e3dc8c6ecbc7289eb10339310ff2fe5978
Cosmeak <guillaumefine09@gmail.com>
2023-04-22 20:05:08 +0200

    feat: server power command

---
+++ src/commands/ClientServerPower.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fecthClientServerInfo = require("../functions/fetchClientServerInfo.js");
+const axios = require("axios");
+require("dotenv").config();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("client-server-power")
+    .setDescription("Provide a way to up or down a server")
+    .addStringOption((option) =>
+      option.setName("server-id")
+        .setDescription("Your server external_ID")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName("state")
+        .setDescription("Choose a state for your server")
+        .setChoices(
+          { name: "start", value: "start" },
+          { name: "stop", value: "stop" },
+          { name: "restart", value: "restart" },
+          { name: "kill", value: "kill" },
+        )
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const state = interaction.options.get("state").value;
+    const id = interaction.options.get("server-id").value;
+    const server = await fecthClientServerInfo(id);
+
+    try {
+      await axios.post(`${process.env.PTERO_HOST}/api/client/servers/${id}/power`, {
+        "headers": {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.PTERO_TOKEN}`,
+        },
+        "body": {
+          "signal": state,
+        }
+      });
+    } catch (error) {
+      console.error(error.response.data.errors);
+      return interaction.reply('An error occurred with your request...');
+    }
+
+    return interaction.reply(`The **${state}** state has been sent to the server **${server.name}**`);
+  }
+};
